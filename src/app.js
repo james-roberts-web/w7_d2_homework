39d86617d892d3cb821a38f9a8a20ea5a8d7b2bd
james-roberts-web <jamesroberts@xponorth.co.uk>
2019-10-15 23:39:28 +0100

    promise return

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-import Vue from 'vue'
+import Vue from 'vue';
 
 document.addEventListener("DOMContentLoaded", () => {
   new Vue({
     el: "#app",
     data:{
       greeting: "Exchange Rates",
-    }
-
+      rates:[]
+    },
+    mounted(){
+      this.getExchangeRate()
+    },
+    methods:{
+      getExchangeRate: function() {
+        const request = fetch("https://api.exchangeratesapi.io/latest");
+        console.log(request);
+      }
+    },
   })
 })
